6974648da77062bf60463a09a803bd615faa0552
Alexandra Dan <dan.alexandra12@yahoo.com>
2023-01-12 15:51:29 +0200

    save

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 var skillsEl = document.getElementById("skills-list");
-//skillsEl.style.fontSize = "25px";
-//skillsEl.style.color = "red";
-
-//var oldText = skillsEl.innerHTML;
-//skillsEl.innerHTML = oldText + "<li>JS</li>";
-
-//skillsEl.innerHTML = skillsEl.innerHTML + "<li>JS</li>";
-
-// skillsEl.innerHTML += '<li class="favorite">HTML</li>';
-// skillsEl.innerHTML += '<li class="favorite">CSS</li>';
-// skillsEl.innerHTML += "<li>JS</li>";
 
 var skills = ["HTML", "CSS", "JS", "Drive"];
-
-// skillsEl.innerHTML += "<li>" + skills[0] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[1] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[2] + "</li>";
 
 var skillsHTML = " ";
 
