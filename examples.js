d956070f637d66d53547927d46d6a7bbc7940fe3
Alexandra Dan <dan.alexandra12@yahoo.com>
2023-01-12 15:42:44 +0200

    extract inline js into  examples.js

---
+++ examples.js
@@ -0,0 +1,22 @@
+console.info("page loaded");
+console.debug(2 + 3);
+console.warn(2 * (3 + 1));
+var employed = true;
+console.error(employed);
+
+var age = 24;
+
+var user = "Alexandra";
+user = user + " Gabriela";
+
+var skills = ["html", "css", "js"];
+
+var alex = {
+  age: 24,
+  employed: true,
+};
+var gabi = {
+  age: 24,
+  employed: true,
+};
+var jobTitle = document.getElementById("job-title");
