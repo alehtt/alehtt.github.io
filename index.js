5c3cc1e5c7aaac3f43af83814497d446a50167a8
Alexandra Dan <dan.alexandra12@yahoo.com>
2023-01-12 15:01:22 +0200

    First version of our CV

---
+++ index.js
@@ -0,0 +1,36 @@
+var skillsEl = document.getElementById("skills-list");
+//skillsEl.style.fontSize = "25px";
+//skillsEl.style.color = "red";
+
+//var oldText = skillsEl.innerHTML;
+//skillsEl.innerHTML = oldText + "<li>JS</li>";
+
+//skillsEl.innerHTML = skillsEl.innerHTML + "<li>JS</li>";
+
+// skillsEl.innerHTML += '<li class="favorite">HTML</li>';
+// skillsEl.innerHTML += '<li class="favorite">CSS</li>';
+// skillsEl.innerHTML += "<li>JS</li>";
+
+var skills = ["HTML", "CSS", "JS", "Drive"];
+
+// skillsEl.innerHTML += "<li>" + skills[0] + "</li>";
+// skillsEl.innerHTML += "<li>" + skills[1] + "</li>";
+// skillsEl.innerHTML += "<li>" + skills[2] + "</li>";
+
+var skillsHTML = " ";
+var i = 0;
+
+// skillsHTML += "<li>" + skills[i] + "</li>";
+// i++;
+// skillsHTML += "<li>" + skills[i] + "</li>";
+// i++;
+// skillsHTML += "<li>" + skills[i] + "</li>";
+// i++;
+// skillsHTML += "<li>" + skills[i] + "</li>";
+// i++;
+
+for (var i = 0; i < skills.length; i++) {
+  skillsHTML += "<li>" + skills[i] + "</li>";
+}
+
+skillsEl.innerHTML = skillsHTML;
